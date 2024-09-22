434730fc99c7296e341dec369fe01940cb15907b
p-ifka <watercereal11@gmail.com>
2024-09-21 23:31:01 -0400

    formatting + defvar section

--- OI.el
+++ OI.el
@@ -5,38 +5,42 @@
 ;; Modified: September 20, 2024
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "24.4"))
-;;
+;; Homepage: https://github.com/p-ifka/robot-code-parser.el
 ;; This file is not part of GNU Emacs.
-;;
-;;  Description
-;;      Function for reading code from robotContainer.java and printing
-;;; Code:
-;;;
-;;;
+(defvar robot-container)
+(defvar robot-container-contents)
+(defvar robot-buttons)
+(defvar r-button)
+(defvar r-command)
 
-(defun OI-get-robot-buttons (&optional ROBOTCONTAINER)
+
+(defun OI-get-robotContainer-buttons (&optional ROBOTCONTAINER)
 "Takes path of robotContainer.java 'ROBOTCONTAINER' and parses button mappings"
 (interactive)
 (if (not ROBOTCONTAINER)
-  (setq ROBOTCONTAINER (read-file-name "robotContainer.java : " )))
+  (setq ROBOTCONTAINER (read-file-name "robotContainer.java : " ))) ;; if 'ROBOTCONTAINER' is not provided, ask for it
 
 (with-temp-buffer
   (insert-file-contents robot-container)
-  (setq robot-container-contents (split-string (buffer-string) "\n")))
+  (setq robot-container-contents (split-string (buffer-string) "\n"))) ;; get contents of file 'ROBOTCONTAINER'
 
 (setq robot-buttons ())
 (dolist (item robot-container-contents)
   (if (> (length item) 6)
-      (if (equal (substring (string-trim-left item) 0 2) "OI")
+      (if (equal (substring (string-trim-left item) 0 2) "OI") ;; get all lines starting with 'OI'
           (progn
-            (setq rbutton (nth 1 (split-string item "\\.")))
-            (setq rcommand
+            (setq r-button (nth 1 (split-string item "\\."))) ;; get button
+            (setq r-command ;; get command
                   (if (string-match "new" item )
                   (substring item
                              (string-match "new" item)
                              (length item))
                   '("runOnceCommand")))
-            (setq robot-buttons (push (list rbutton rcommand) robot-buttons))
-(print robot-buttons))))))
-
+            (setq robot-buttons (push (list r-button r-command) robot-buttons)) ;; push button and command to list
+(print robot-buttons)))))) ;; TODO: use better output method
+;; ====================================================================================================
+;;
+;; ====================================================================================================
+;;TODO : add function for getting hardware values from 'OI.java'
+;;TODO : add optional file output into 'README.md'
 (provide 'OI)
