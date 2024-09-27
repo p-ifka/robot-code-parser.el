ff4a17045f3a3a947bd433a3209cc4cebb0fda1d
p-ifka <watercereal11@gmail.com>
2024-09-26 20:21:05 -0400

    added sorting by map type

--- OI.el
+++ OI.el
@@ -7,21 +7,20 @@
 ;; Package-Requires: ((emacs "24.4"))
 ;; Homepage: https://github.com/p-ifka/robot-code-parser.el
 ;; This file is not part of GNU Emacs.
-(defvar robot-container)
 (defvar robot-container-contents)
 (defvar robot-buttons)
 (defvar r-button)
 (defvar r-command)
 
 
-(defun OI-get-robotContainer-buttons (&optional ROBOTCONTAINER)
+(defun OI-get (&optional DIR)
 "Takes path of robotContainer.java 'ROBOTCONTAINER' and parses button mappings"
 (interactive)
-(if (not ROBOTCONTAINER)
-  (setq ROBOTCONTAINER (read-file-name "robotContainer.java : " ))) ;; if 'ROBOTCONTAINER' is not provided, ask for it
+(if (not DIR)
+  (setq DIR (read-directory-name "/robot : " ))) ;; if 'ROBOTCONTAINER' is not provided, ask for it
 
 (with-temp-buffer
-  (insert-file-contents robot-container)
+  (insert-file-contents (concat DIR "/" "RobotContainer.java"))
   (setq robot-container-contents (split-string (buffer-string) "\n"))) ;; get contents of file 'ROBOTCONTAINER'
 
 (setq robot-buttons ())
@@ -31,16 +30,89 @@
           (progn
             (setq r-button (nth 1 (split-string item "\\."))) ;; get button
             (setq r-command ;; get command
-                  (if (string-match "new" item )
+                  (if (string-match "new" item ) ;; get command
                   (substring item
                              (string-match "new" item)
                              (length item))
-                  '("runOnceCommand")))
+                  (if (string-match "{" item) ;; get multiline runOnceCommand
+                      "multiline runOnceCommand"
+                      ;; TODO: get all lines below until ending }
+
+                  (substring item ;; get single line runOnceCommand
+                             ( -(string-match ">" item) 1)
+                             (-(length item) 1)))))
             (setq robot-buttons (push (list r-button r-command) robot-buttons)) ;; push button and command to list
-(print robot-buttons)))))) ;; TODO: use better output method
+))))
+;; == Add Map Type From OI == ;;
+  (with-temp-buffer
+    (insert-file-contents (concat DIR "/" "OI.java"))
+    (setq oi-contents (split-string (buffer-string) "\n")))
+  (setq cmd-i 0)
+  (dolist (cmd robot-buttons)
+    (dolist (line oi-contents)
+      (if (string-match (nth 0 cmd) line)
+
+          ;; push either 'Driver Map' or 'Operator Map'
+          (if (string-match "DRIVER_MAP" line )
+              (push "Driver Map" (nth cmd-i robot-buttons))
+            (if (string-match "OPERATOR_MAP" line)
+                (push "Operator Map" (nth cmd-i robot-buttons))
+              (push "[err: command map type not found]" (nth cmd-i robot-buttons)))))
+)(setq cmd-i (+ cmd-i 1)))
+
+(if (yes-or-no-p "output to buffer?: ")
+    (OI-output robot-buttons)
+    ))
 ;; ====================================================================================================
 ;;
 ;; ====================================================================================================
-;;TODO : add function for getting hardware values from 'OI.java'
-;;TODO : add optional file output into 'README.md'
+
+;; -- Buffer Print -- ;;
+(defun OI-output (OUTPUT)
+ (setq output-buffer (generate-new-buffer "*OI-output*"))
+ (set-buffer output-buffer)
+(setq driver-map "")
+(setq operator-map "")
+ (dolist (item OUTPUT)
+   (setq rcommand-name (substring (nth 2 item) 3 (cl-position 40 (string-to-list (nth 2 item))) ))
+   (setq rargs (substring (nth 2 item)
+        (cl-position 40 (string-to-list (nth 2 item)))
+        (- (length (nth 2 item)) 2)))
+   (if (equal (nth 0 item) "Driver Map")
+       (setq driver-map (concat driver-map "- " (nth 1 item) " - " rcommand-name ": " rargs "\n"))
+        (setq operator-map (concat operator-map "- " (nth 1 item) " - " rcommand-name ": " rargs "\n"))
+        )) ;; end of dolist
+(insert (concat
+         "#+title: OI.el output\n"
+         "* Operator\n"
+         operator-map
+         "* Driver\n"
+         driver-map))
+(org-mode)
+(setq buffer-read-only t))
+    ;; (insert (concat "*" (nth 0 item) "*" " - " rcommand-name ": " rargs "\n"))
+
+;; ====================================================================================================
+;;
+;; ====================================================================================================
 (provide 'OI)
+
+;; (progn
+;;   (with-temp-buffer
+;;     (insert-file-contents (expand-file-name "~/src/LER-2024/src/main/java/frc/robot/OI.java"))
+;;     (setq oi-contents (split-string (buffer-string) "\n")))
+;;   (setq cmd-i 0)
+;;   (dolist (cmd robot-buttons)
+;;     (dolist (line oi-contents)
+;;       (if (string-match (nth 0 cmd) line)
+
+;;           ;; push either 'Driver Map' or 'Operator Map'
+;;           (if (string-match "DRIVER_MAP" line )
+;;               (push "Driver Map" (nth cmd-i robot-buttons))
+;;             (if (string-match "OPERATOR_MAP" line)
+;;                 (push "Operator Map" (nth cmd-i robot-buttons))
+;;               (push "[err: command map type not found]" (nth cmd-i robot-buttons)))))
+
+
+;; )(setq cmd-i (+ cmd-i 1)))
+;; (print robot-buttons))
